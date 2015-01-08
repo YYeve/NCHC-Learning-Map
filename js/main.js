58f40436e465e61a2f2fad1565ac9f14256ad326
YYeve <e78v03e10eve@hotmail.com>
2015-01-08 14:17:21 +0800

    天文動畫
    好lag阿~~

--- js/main.js
+++ js/main.js
@@ -18,45 +18,106 @@ $(function() {
    // This option accepts a callback function. The function will be called before the page moves.
    afterMove: function(index) {
 	   	//set delay and display animation
-		$(".icons").addClass('wow fadeInDown animated').delay(1000).queue(function(){
-			$(this).removeClass("wow fadeInDown animated").dequeue();
+		$(".icons").addClass('wow bounce animated').delay(1000).queue(function(){
+			$(this).removeClass("wow bounce animated").dequeue();
 		});
 		
-	   	$(".item-star1").addClass('wow bounceIn animated').delay(1000).queue(function(){
+		//星星1
+		setTimeout( function() {
+			$(".item-star1").show();
+			$(".item-star1").addClass('wow bounceIn animated').delay(1000).queue(function(){
   			 $(this).removeClass("wow bounceIn animated").dequeue();
-  		});
-		$(".item-star2").addClass('wow bounceIn animated').delay(1000).queue(function(){
+  			});
+		},500);
+		
+		//星星2
+		setTimeout( function() {
+			$(".item-star2").show();
+			$(".item-star2").addClass('wow bounceIn animated').delay(1000).queue(function(){
   			 $(this).removeClass("wow bounceIn animated").dequeue();
-  		});
-		$(".item-plante1").addClass('wow rubberBand animated').delay(1000).queue(function(){
+  			});
+		},1000);
+		
+		//星星3
+		setTimeout( function() {
+			$(".item-star3").show();
+			$(".item-star3").addClass('wow bounceIn animated').delay(1000).queue(function(){
+  			 $(this).removeClass("wow bounceIn animated").dequeue();
+  			});
+		},1500);
+		
+		//plante1 黃星球
+		setTimeout( function() {
+			$(".item-plante1").show();
+			$(".item-plante1").addClass('wow rubberBand animated').delay(1000).queue(function(){
   			 $(this).removeClass("wow rubberBand animated").dequeue();
   		});
-		$(".item-plante2").addClass('wow rubberBand animated').delay(1000).queue(function(){
+		},600);
+		
+		//plante2 綠星球
+		setTimeout( function() {
+			$(".item-plante2").show();
+			$(".item-plante2").addClass('wow rubberBand animated').delay(1000).queue(function(){
   			 $(this).removeClass("wow rubberBand animated").dequeue();
   		});
+		},1500);
 		
-		
+		//plante3 菊星球
 		setTimeout( function() {
 			$(".item-plante3").show();
 			$(".item-plante3").addClass('wow rubberBand animated').delay(1000).queue(function(){
 				 $(this).removeClass("wow rubberBand animated").dequeue();
 			});
-		},500);
+		},2400);
 		
-		$(".item-plante4").addClass('wow rubberBand animated').delay(1000).queue(function(){
+		//plante2 紫星球
+		setTimeout( function() {
+			$(".item-plante4").show();
+			$(".item-plante4").addClass('wow rubberBand animated').delay(1000).queue(function(){
   			 $(this).removeClass("wow rubberBand animated").dequeue();
   		});
-		$(".item-star3").addClass('wow bounceIn animated').delay(1000).queue(function(){
-  			 $(this).removeClass("wow bounceIn animated").dequeue();
-  		});
+		},800);
 		
-		
-		
+		//ET
 		setTimeout( function(){
 			$(".item-ET").show();
 			$(".item-ET").addClass('wow rollIn animated').delay(1000).queue(function(){
 				 $(this).removeClass("wow rollIn animated").dequeue();
 			});}, 1000);
+			
+		//astronomy	
+		setTimeout( function(){
+		$(".astronomy_english").show();
+		$(".astronomy_english").addClass('wow fadeInDown animated').delay(1000).queue(function(){
+  		 $(this).removeClass("wow fadeInDown animated").dequeue();
+  		});
+		},500);
+		
+		//天文	
+		setTimeout( function(){
+		$(".astronomy_title").show();
+		$(".astronomy_title").addClass('wow fadeInUp animated').delay(1000).queue(function(){
+  		 $(this).removeClass("wow fadeInUp animated").dequeue();
+  		});
+		},1000);
+		
+		//天文icon	
+		setTimeout( function(){
+		$(".astronomy_icon").show();
+		$(".astronomy_icon").addClass('wow fadeInRight animated').delay(1000).queue(function(){
+  		 $(this).removeClass("wow fadeInRight animated").dequeue();
+  		});
+		},1500);
+		
+		//天文介紹文字	
+		setTimeout( function(){
+		$(".astronomy_intro").show();
+		$(".astronomy_intro").addClass('wow fadeIn animated').delay(1000).queue(function(){
+  		 $(this).removeClass("wow fadeIn animated").dequeue();
+  		});
+		},2000);
+			
+			
 		
 	   },   // This option accepts a callback function. The function will be called after the page moves.
    loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
